622cee416b4ba2e54dd72470317f442edb200b28
sherlynkek <sherlynkek00@gmail.com>
2024-10-23 12:05:48 +0800

    Task10

--- Task10.java
+++ Task10.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task10 {
-    
+    public static void main(String[] args) {
+        
+        // Random generated number 
+        Random random = new Random();
+        List<Integer> numList = new ArrayList<>();
+        
+        for (int i = 0; i < 10; i++) {
+            numList.add(random.nextInt(1, 100));
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        List<String> guessList = new ArrayList<>();
+        
+
+
+    }
 }
